57bc604001707a97519de2910f7f47571d200da5
Germano Gomes <germano-software-dev@hotmail.com>
2022-07-27 07:13:04 -0300

    feat: Update application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SharedModule } from './modules/shared/shared.module';
+import { ApiConfigService } from './shared/services/api-config.service';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
