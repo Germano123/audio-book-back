dbb15af1453ddecffb8e8a461e627feb5b301b34
Germano <germanomgomes@hotmail.com>
2022-07-27 23:51:21 -0300

    feat: Add common files for application.

--- src/common/abstract.entity.ts
+++ src/common/abstract.entity.ts
@@ -1,30 +1,34 @@
 'use strict';
 
-import { Exclude } from "@nestjs/class-transformer";
-import { UtilsService } from "src/providers/utils.service";
-import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { AbstractDto } from "./dto/AbstractDto";
+import { Exclude } from '@nestjs/class-transformer';
+import { UtilsService } from '../providers/utils.service';
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { AbstractDto } from './dto/AbstractDto';
 
 export abstract class AbstractEntity<T extends AbstractDto = AbstractDto> {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @CreateDateColumn({
-        type: 'timestamp without time zone',
-        name: 'created_at',
-    })
-    createdAt: Date;
+  @CreateDateColumn({
+    type: 'timestamp without time zone',
+    name: 'created_at',
+  })
+  createdAt: Date;
 
-    @UpdateDateColumn({
-        type: 'timestamp without time zone',
-        name: 'updated_at',
-    })
-    updatedAt: Date;
+  @UpdateDateColumn({
+    type: 'timestamp without time zone',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
 
-    @Exclude()
-    abstract dtoClass: new (entity: AbstractEntity, options?: any) => T;
+  @Exclude()
+  abstract dtoClass: new (entity: AbstractEntity, options?: any) => T;
 
-    toDto(options?: any) {
-        return UtilsService.toDto(this.dtoClass, this, options);
-    }
+  toDto(options?: any) {
+    return UtilsService.toDto(this.dtoClass, this, options);
+  }
 }
--- src/common/dto/AbstractDto.ts
+++ src/common/dto/AbstractDto.ts
@@ -3,16 +3,16 @@
 import { AbstractEntity } from '../abstract.entity';
 
 export class AbstractDto {
-    // @ApiPropertyOptional()
-    id: string;
-    // @ApiPropertyOptional()
-    createdAt: Date;
-    // @ApiPropertyOptional()
-    updatedAt: Date;
+  // @ApiPropertyOptional()
+  id: string;
+  // @ApiPropertyOptional()
+  createdAt: Date;
+  // @ApiPropertyOptional()
+  updatedAt: Date;
 
-    constructor(entity: AbstractEntity) {
-        this.id = entity.id;
-        this.createdAt = entity.createdAt;
-        this.updatedAt = entity.updatedAt;
-    }
+  constructor(entity: AbstractEntity) {
+    this.id = entity.id;
+    this.createdAt = entity.createdAt;
+    this.updatedAt = entity.updatedAt;
+  }
 }
